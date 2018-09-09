dd49bb39bafb4754ff52f44347f90deccd5628d9
Aleksei Vozhdaev <soulburglar@gmail.com>
2018-09-09 19:37:18 +0300

    instance of redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,52 @@
 import 'bootstrap';
 import './css/index.css';
+import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './js/App';
+// import Counter from './js/Counter';
+// import App from './js/App';
 
-ReactDOM.render(
-	<App />, document.getElementById('app')
-)
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => {
+  return(
+    <div>
+      <h1>{value}</h1>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+    </div>
+  )
+};
+
+const store = createStore(counter);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({
+        type: 'INCREMENT'
+      })}
+      onDecrement={() => store.dispatch({
+        type: 'DECREMENT'
+      })}
+    />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
